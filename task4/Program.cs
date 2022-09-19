4f675a647a4cc2b35dae7d3aab30036797e89c3a
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-09-19 17:45:45 +0300

    Решение задачи 4

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = 1;
+if (numberA > numberB) 
+{
+Console.WriteLine("max = numberA");
+}
+if (numberB > numberC) 
+{
+Console.WriteLine("max = numberB");
+}
+else 
+{
+Console.WriteLine(max = numberC);
+}
+
