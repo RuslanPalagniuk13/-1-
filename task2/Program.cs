4327208c5995d5226bc91cb2b0a79effb7d6ee7c
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-09-19 16:34:13 +0300

    Решение задачи 2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+    {
+        Console.Write("max = "); 
+        Console.WriteLine(numberA);
+    }
+else
+    {
+        Console.Write("max = "); 
+        Console.WriteLine(numberB);
+    }
+if (numberA < numberB)
+    {
+        Console.Write("min = "); 
+        Console.WriteLine (numberA);
+    }
+else
+    {
+        Console.Write("min = ");
+        Console.WriteLine(numberB);
+    }
