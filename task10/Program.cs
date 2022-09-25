8885d43cd78053a1178243dc8e6d3c0cd4236059
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-09-25 19:39:18 +0300

    решение задачи 10

---
+++ task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трехзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.Write("Вторая цифра числа: " +stringNumber[1]);
