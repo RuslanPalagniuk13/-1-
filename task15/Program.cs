d45330cfe4a25471f45dab1fae250166044112dc
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-09-25 20:39:17 +0300

    решение задачи 15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void CheckingTneDayOfTheWeek (int dayNumber) 
+{
+    if (dayNumber == 6 || dayNumber == 7) {
+    Console.WriteLine("(Этот день недели выходной?) -> Да");
+    }
+    else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Это не день недели!");  
+    }
+    else Console.WriteLine("(Этот день недели выходной?) -> Hет");
+}
+
+CheckingTneDayOfTheWeek(dayNumber);
