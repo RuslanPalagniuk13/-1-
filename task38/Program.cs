0466d4d0e4e01cd153b46a3b85a54574361c0a28
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-10-02 11:17:51 +0300

    решение задачи 38

---
+++ task38/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Наш массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int y = 0; y < numbers.Length; y++)
+{
+    if (numbers[y] > max)
+        {
+            max = numbers[y];
+        }
+    if (numbers[y] < min)
+        {
+            min = numbers[y];
+        }
+}
+
+Console.WriteLine($"В масиве всего чисел {numbers.Length}. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
