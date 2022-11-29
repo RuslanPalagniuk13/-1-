0fe4e2d4d5776203287aeae68a7df0f24d514bc2
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-11-29 19:15:57 +0300

    добавлено условие задачи 27

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.Write("Введите число N: ");
+﻿/* Задача 27. Напишите программу, которая принимает на вход число
+и выдает сумму цифр в числе. 
+ Например:
+ 452 -> 11
+ 82 -> 10
+ 9012 -> 12
+ */
+
+Console.Write("Введите число N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 
   int SumNumber(int numberN)
