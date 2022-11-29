d3b6fbdef393c48ca3cda5d4a69e3695604719f6
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-11-29 16:45:48 +0300

    добавлено условие задачи 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.Write("Введите число A: ");
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдает, 
+какое число больше, а какое меньше.
+Пример: 
+a=5; b=7 -> = 7
+a=2; b=10 -> = 10
+a=-9; b=-3 -> = 7 
+*/
+
+Console.Write("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
