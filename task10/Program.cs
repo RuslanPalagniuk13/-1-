93dc0cd6eddf6177d35882cf4d55320288d74347
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-11-29 18:47:53 +0300

    добавлено условие задачи 10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.Write("Введите трехзначное число: ");
+﻿/* Задача 10. Напишите программу, которая принимает на вход трехзначное число
+и проверяет и на выходе показывает вторую цифру этого числа. 
+ Например:
+ 456 -> 5
+ 782 -> 8
+ 918 -> 1
+ */
+
+Console.Write("Введите трехзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
 Console.Write("Вторая цифра числа: " +stringNumber[1]);
