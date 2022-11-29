fa09a25747092dbcedbfd59ef5f32430ecdc3a4d
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-11-29 19:32:04 +0300

    добавлено условие задачи 36

--- task36/Program.cs
+++ task36/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.WriteLine("Введите размер массива");
+﻿/* Задача 36. Задайте одномерный массив, заполненых чисел.
+Найдите разницу между максимальным и минимальным элементом массива. 
+ Например:
+ 3, 7, 23, 12 -> 19
+ -4, -6, 89, 6 -> 0
+ */
+
+Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
